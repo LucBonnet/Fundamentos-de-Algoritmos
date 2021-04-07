027546832e493fc68beaa73764f5ee2a4d779e87
JEAN LUC BONNET <jean.bonnet@etec.sp.gov.br>
2021-04-07 20:11:47 -0300

    Aula 10

--- Aula10/Exercicios/ex3.py
+++ Aula10/Exercicios/ex3.py
@@ -16,9 +16,9 @@
 def produtorio(n1, n2, n3, n4=1, n5=1, n6=1):
   return n1 * n2 * n3 * n4 * n5 * n6
 
-## ou
+## ou (função que recebe infinitos valores)
 
-def produt(*args):
+def prod(*args):
   result = 1
   for num in args:
     result *= num
